c3c279dfbcc8454aa0bb2843a6edd7bf1a14a93a
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-27 17:02:35 +0300

    Initial Commit

---
+++ module3lesson8GeometryReaderEventGestureHomeWork/module3lesson8GeometryReaderEventGestureHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3lesson8GeometryReaderEventGestureHomeWorkApp.swift
+//  module3lesson8GeometryReaderEventGestureHomeWork
+//
+//  Created by Максим Минаков on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3lesson8GeometryReaderEventGestureHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
